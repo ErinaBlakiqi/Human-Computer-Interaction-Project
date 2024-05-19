f79e1bbf2ab418ec562cc8ac5cc9713c2a1e20ba
roniveseli1 <roni.veseli@student.uni-pr.edu>
2024-05-19 23:53:46 +0200

    Update the home page design

---
+++ src/main/java/controllers/CardProductBestSellerController.java
@@ -0,0 +1,28 @@
+package controllers;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class CardProductBestSellerController implements Initializable {
+    @FXML
+    private AnchorPane card_form_bestseller;
+
+    @FXML
+    private ImageView prod_imageView;
+
+    @FXML
+    private Label prod_name;
+
+    @FXML
+    private Label prod_price;
+
+    //private productData prodData;
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+}

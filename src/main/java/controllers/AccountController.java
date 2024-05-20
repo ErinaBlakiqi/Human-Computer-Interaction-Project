62ea787fd9579d8f2161bd2e96365bf158db80c1
EreaBlakaj <ereablakaj@gmail.com>
2024-05-20 22:36:16 +0200

    Addition to Account Controller

--- src/main/java/controllers/AccountController.java
+++ src/main/java/controllers/AccountController.java
@@ -1,15 +1,46 @@
 package controllers;
 
+import application.Navigator;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import javafx.scene.shape.Rectangle;
 
 public class AccountController {
+    @FXML
+    private Button adminButton;
 
+    @FXML
+    private Button homeButton;
+
+    @FXML
+    private Label nameLabel;
+
+    @FXML
+    private Button productsButton;
+
+    @FXML
+    private ImageView profileImage;
+
+    @FXML
+    private Rectangle rectId;
+
+    @FXML
+    private Button sellButton;
+
+    @FXML
+    private Button signoutButton;
+
+    @FXML
+    private Button userButton;
+
+    @FXML
+    private Label welcomeLabel;
     // Buttons
     @FXML private Button sellItemButton;
     @FXML private Button cartButton;
@@ -34,13 +65,38 @@ public class AccountController {
     @FXML private ScrollPane itemsScrollPane;
     @FXML private ScrollPane collectionsScrollPane;
 
+    @FXML
+    void handleAdmin(MouseEvent event) {
+        Navigator.navigate(event, Navigator.ADMIN_PAGE);
+    }
+
+    @FXML
+    void handleHome(MouseEvent event) {
+        Navigator.navigate(event, Navigator.HOME_PAGE);
+    }
+
+    @FXML
+    void handleProducts(MouseEvent event) {
+        Navigator.navigate(event, Navigator.PRODUCTS_PAGE);
+    }
+
+    @FXML
+    void handleSell(MouseEvent event) {
+        Navigator.navigate(event, Navigator.SELL_PAGE);
+    }
+
+    @FXML
+    void handleSignOut(MouseEvent event) {
+        Navigator.navigate(event, Navigator.SIGNIN_PAGE);
+    }
+
+    @FXML
+    void handleUser(MouseEvent event) {
+        Navigator.navigate(event, Navigator.USER_PAGE);
+    }
     // Event handlers for the UI interactions
     @FXML private void handleSellItem() {
     }
-
-    @FXML private void handleCart() {
-    }
-
     @FXML private void handleEditAccount() {
     }
     @FXML

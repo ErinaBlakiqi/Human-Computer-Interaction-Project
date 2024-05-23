369936f1cf9b03cb7e26feb5b2f846950f15e298
EreaBlakaj <ereablakaj@gmail.com>
2024-05-23 22:17:52 +0200

    Implemented editAccount window

--- src/main/java/controllers/AccountController.java
+++ src/main/java/controllers/AccountController.java
@@ -8,7 +8,6 @@ import javafx.scene.control.ScrollPane;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.HBox;
-import javafx.scene.layout.VBox;
 import javafx.scene.shape.Rectangle;
 
 public class AccountController {
---
+++ src/main/java/controllers/editAccountController.java
@@ -0,0 +1,43 @@
+package controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+
+public class editAccountController {
+
+    @FXML
+    private TextField addressField;
+
+    @FXML
+    private TextArea bioField;
+
+    @FXML
+    private Button changeProfilePictureButton;
+
+    @FXML
+    private TextField contactNumberField;
+
+    @FXML
+    private ImageView profileImageView;
+
+    @FXML
+    private Button saveButton;
+
+    @FXML
+    private TextField usernameField;
+
+    @FXML
+    void handleChangeProfilePicture(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handleEditAccountSave(ActionEvent event) {
+
+    }
+
+}

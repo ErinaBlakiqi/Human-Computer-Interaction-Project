26cad20e5d9915563123a77c9871ee1bc9a0567e
Erina Blakiqi <blakiqierina@gmail.com>
2024-05-24 18:40:59 +0200

    Create Category.java

---
+++ src/main/java/model/Category.java
@@ -0,0 +1,24 @@
+package model;
+
+public class Category {
+    private int categoryId;
+    private String cName;
+
+    // Getters
+    public int getCategoryId() {
+        return categoryId;
+    }
+
+    public String getCName() {
+        return cName;
+    }
+
+    // Setters
+    public void setCategoryId(int categoryId) {
+        this.categoryId = categoryId;
+    }
+
+    public void setCName(String cName) {
+        this.cName = cName;
+    }
+}

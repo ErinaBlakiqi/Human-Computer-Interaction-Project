57eed84c003c2d2ea7c72d686964678d235f0b14
Erina Blakiqi <blakiqierina@gmail.com>
2024-05-24 18:41:07 +0200

    Create Cart.java

---
+++ src/main/java/model/Cart.java
@@ -0,0 +1,42 @@
+package model;
+
+public class Cart {
+    private int cartId;
+    private int userId;
+    private int productId;
+    private int quantity;
+
+    // Getters
+    public int getCartId() {
+        return cartId;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public int getProductId() {
+        return productId;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    // Setters
+    public void setCartId(int cartId) {
+        this.cartId = cartId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public void setProductId(int productId) {
+        this.productId = productId;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+}

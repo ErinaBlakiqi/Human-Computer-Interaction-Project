269b7c97c3e0469b10327a8c76644824291e4874
EreaBlakaj <ereablakaj@gmail.com>
2024-05-26 19:15:48 +0200

    updates

---
+++ src/main/java/model/dto/ProfileSellDto.java
@@ -0,0 +1,41 @@
+package model.dto;
+
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+
+public class ProfileSellDto {
+    private final StringProperty date;
+    private final StringProperty item;
+    private final SimpleIntegerProperty price;
+
+    public ProfileSellDto(String date, String item, int price) {
+        this.date = new SimpleStringProperty(date);
+        this.item = new SimpleStringProperty(item);
+        this.price = new SimpleIntegerProperty(price);
+    }
+
+    public String getDate() {
+        return date.get();
+    }
+
+    public StringProperty dateProperty() {
+        return date;
+    }
+
+    public String getItem() {
+        return item.get();
+    }
+
+    public StringProperty itemProperty() {
+        return item;
+    }
+
+    public int getPrice() {
+        return price.get();
+    }
+
+    public SimpleIntegerProperty priceProperty() {
+        return price;
+    }
+}
